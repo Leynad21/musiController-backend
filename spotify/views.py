b395f890c62eee746943fe4a101248b1d919675e
Dany <dany21sayan_@hotmail.com>
2023-07-23 05:18:29 +0200

    before bug

---
+++ spotify/views.py
@@ -0,0 +1,165 @@
+from django.shortcuts import render, redirect
+from rest_framework.views import APIView
+from requests import Request, post
+from rest_framework import status
+from rest_framework.response import Response
+from .util import update_or_create_user_tokens, is_spotify_authenticated, get_user_tokens, execute_spotify_api_request, play_song, pause_song, skip_song
+from api.models import Room
+from .models import Vote
+import environ
+from pathlib import Path
+
+env = environ.Env(DEBUG=(bool, False))
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+environ.Env.read_env(BASE_DIR / ".env")
+
+
+class AuthURL(APIView):
+    def get(self, request, fornat=None):
+        scopes = 'user-read-playback-state user-modify-playback-state user-read-currently-playing'
+
+        url = Request('GET', 'https://accounts.spotify.com/authorize', params={
+            'scope': scopes,
+            'response_type': 'code',
+            'redirect_uri': env("REDIRECT_URI"),
+            'client_id': env("CLIENT_ID")
+        }).prepare().url
+
+        return Response({'url': url}, status=status.HTTP_200_OK)
+
+
+def spotify_callback(request, format=None):
+    code = request.GET.get('code')
+    error = request.GET.get('error')
+
+    response = post('https://accounts.spotify.com/api/token', data={
+        'grant_type': 'authorization_code',
+        'code': code,
+        'redirect_uri': env("REDIRECT_URI"),
+        'client_id': env("CLIENT_ID"),
+        'client_secret': env("CLIENT_SECRET")
+    }).json()
+
+    access_token = response.get('access_token')
+    token_type = response.get('token_type')
+    refresh_token = response.get('refresh_token')
+    expires_in = response.get('expires_in')
+    error = response.get('error')
+
+    if not request.session.exists(request.session.session_key):
+        request.session.create()
+
+    update_or_create_user_tokens(
+        request.session.session_key, access_token, token_type, expires_in, refresh_token)
+
+    return redirect('http://127.0.0.1:5173/')
+
+
+class IsAuthenticated(APIView):
+    def get(self, request, format=None):
+        is_authenticated = is_spotify_authenticated(
+            self.request.session.session_key)
+        return Response({'status': is_authenticated}, status=status.HTTP_200_OK)
+    
+class CurrentSong(APIView):
+    def get(self, request, format=None):
+        room_code = self.request.session.get('room_code')
+        room = Room.objects.filter(code=room_code)
+        print("roo_codem in view: ", room_code)
+        if room.exists():
+            room = room[0]
+        else:
+            return Response({"error": "room not found"}, status=status.HTTP_404_NOT_FOUND)
+        host = room.host
+        print("host in view: ", host)
+        endpoint = "player/currently-playing"
+        response = execute_spotify_api_request(host, endpoint)
+
+        if 'error' in response or 'item' not in response:
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+        item = response.get('item')
+        duration = item.get('duration_ms')
+        progress = response.get('progress_ms')
+        album_cover = item.get('album').get('images')[0].get('url')
+        is_playing = response.get('is_playing')
+        song_id = item.get('id')
+
+        artist_string = ""
+
+        for i, artist in enumerate(item.get('artists')):
+            if i > 0:
+                artist_string += ", "
+            name = artist.get('name')
+            artist_string += name
+
+        votes = len(Vote.objects.filter(room=room, song_id=song_id))
+        song = {
+            'title': item.get('name'),
+            'artist': artist_string,
+            'duration': duration,
+            'time': progress,
+            'image_url': album_cover,
+            'is_playing': is_playing,
+            'votes': votes,
+            'votes_required': room.votesToSkip,
+            'id': song_id
+        }
+
+        self.update_room_song(room, song_id)
+
+        return Response(song, status=status.HTTP_200_OK)
+    
+    def update_room_song(self, room, song_id):
+        current_song = room.currentSong
+
+        if current_song != song_id:
+            room.currentSong = song_id
+            room.save(update_fields=['currentSong'])
+            votes = Vote.objects.filter(room=room).delete()
+    
+
+    
+class PauseSong(APIView):
+    def put(self, response, format=None):
+        room_code = self.request.session.get('room_code')
+        room = Room.objects.filter(code=room_code)[0]
+        if self.request.session.session_key == room.host or room.guestCanPause:
+            pause_song(room.host)
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+        
+        return Response({}, status=status.HTTP_403_FORBIDDEN)
+    
+class PlaySong(APIView):
+    def put(self, response, format=None):
+        room_code = self.request.session.get('room_code')
+        room = Room.objects.filter(code=room_code)[0]
+        if self.request.session.session_key == room.host or room.guest_can_pause:
+            play_song(room.host)
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+        
+        return Response({}, status=status.HTTP_403_FORBIDDEN)
+    
+
+class SkipSong(APIView):
+    def post(self, request, format=None):
+        room_code = self.request.session.get('room_code')
+        room = Room.objects.filter(code=room_code)[0]
+        votes = Vote.objects.filter(room=room, song_id=room.currentSong)
+        votes_needed = room.votesToSkip
+        if self.request.session.session_key == room.host or len(votes) + 1 >= votes_needed:
+            votes.delete()
+            skip_song(room.host)
+        else:
+        
+            vote = Vote(user=self.request.session.session_key,
+                        room=room, song_id=room.currentSong)
+            if vote:
+                print("Cannot vote twice")
+                print(vote)
+            else:
+                vote.save()
+                print(vote)
+
+        return Response({}, status.HTTP_204_NO_CONTENT)
